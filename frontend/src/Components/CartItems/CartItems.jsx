f5779bf1bc70ee7e89e9437695251e526e2bbced
Atharva Dhurwey <atharva.dhurwey.2000@gmail.com>
2024-01-24 23:06:19 +0530

    added cart page

--- frontend/src/Components/CartItems/CartItems.jsx
+++ frontend/src/Components/CartItems/CartItems.jsx
@@ -1,79 +1,111 @@
 import React, { useContext } from 'react';
 import './CartItems.css';
 import { ShopContext } from '../../Context/ShopContext';
-import remove_icon from '../Assets/cart_cross_icon.png';
+import Breadcrum from '../Breadcrums/Breadcrum';
+import { Link } from 'react-router-dom';
 
 const CartItems = () => {
   const { all_product, cartItems, removeFromCart, getTotalCartAmount } = useContext(ShopContext);
   return (
-    <div className="cartitems">
-      <div className="cartitems-format-main">
-        <p>Products</p>
-        <p>Title</p>
-        <p>Price</p>
-        <p>Quantity</p>
-        <p>Total</p>
-        <p>Remove</p>
-      </div>
-      <hr />
-      {all_product.map((e) => {
-        if (cartItems[e.id] > 0) {
-          return (
-            <div key={e.id}>
-              <div className="cartitems-format cartitems-format-main">
-                <img src={e.image} alt="" className="carticon-product-icon" />
-                <p>{e.name}</p>
-                <p>${e.new_price}</p>
-                <button className="cartitems-quantity">
-                  {cartItems[e.id]}
-                </button>
-                <p>${e.new_price * cartItems[e.id]}</p>
-                <img
-                  className="cartitems-remove-icon"
-                  src={remove_icon}
-                  onClick={() => {
-                    removeFromCart(e.id);
-                  }}
-                  alt=""
-                />
+    all_product.length > 0 && (
+      <>
+        <Breadcrum crum={'HOME / SHOP '} name={'Cart items'} banner={'cart'} />
+
+        <section className="shoppingCart-main-container" id="cartList">
+          <h1 className="shoppingCart-main-container-heading">Products</h1>
+          <div className="shoppingCart-productDetail-container">
+            <div className="shoppingCart-product-container">
+              <div className="shoppingCart-product-items">
+                <div className="shoppingCart-product-container-line"></div>
+
+                {getTotalCartAmount() === 0 ? (
+                  <div className="shoppingCart-product-empty">
+                    Your Cart is <span>EMPTY</span>
+                  </div>
+                ) : null}
+
+                {all_product.map((e) => {
+                  if (cartItems[e.id] > 0) {
+                    return (
+                      <div key={e.id}>
+                        <div className="shoppingCart-product-item">
+                          <div className="shoppingCart-product-image">
+                            <img src={e.image} loading="lazy" alt="" />
+                          </div>
+                          <div className="shoppingCart-product-name">
+                            <div>Name</div>
+                            <div>{e.name}</div>
+                          </div>
+                          <div className="shoppingCart-product-quantity">
+                            <div>Quantity</div>
+                            <div className="shoppingCart-product-quantity-input">{cartItems[e.id]}</div>
+                          </div>
+                          <div className="shoppingCart-product-price">
+                            <div>Price</div>
+                            <div>${e.new_price * cartItems[e.id]}</div>
+                          </div>
+                          <div className="shoppingCart-product-delete">
+                            <button
+                              onClick={() => {
+                                removeFromCart(e.id);
+                              }}
+                              className="delete-item"
+                            >
+                              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                                <path d="M3 3h18v18H3zM15 9l-6 6m0-6l6 6" />
+                              </svg>
+                            </button>
+                          </div>
+                        </div>
+                        <div className="shoppingCart-product-container-line"></div>
+                      </div>
+                    );
+                  } else {
+                    return null;
+                  }
+                })}
               </div>
-              <hr />
             </div>
-          );
-        } else {
-          return null;
-        }
-      })}
-      <div className="cartitems-down">
-        <div className="cartitems-total">
-          <h1>Cart Totals</h1>
-          <div>
-            <div className="cartitems-total-item">
-              <p>Subtotal</p>
-              <p>${getTotalCartAmount()}</p>
-            </div>
-            <hr />
-            <div className="cartitems-total-item">
-              <p>Shipping Fee</p>
-              <p>Free</p>
-            </div>
-            <hr />
-            <div className="cartitems-total-item">
-              <h3>Total</h3>
-              <h3>${getTotalCartAmount()}</h3>
+
+            <div className="shoppingCart-productPrice-container">
+              <div className="shoppingCart-productPrice-content">
+                <h1 className="shoppingCart-productPrice-heading">Cart Total</h1>
+                <div className="shoppingCart-productPrice-subtotal">
+                  <h1>Subtotal:</h1>
+                  <h1 className="shoppingCart-productPrice-price">${getTotalCartAmount()}</h1>
+                </div>
+                <h1 className="shoppingCart-productPrice-heading">Shipping:</h1>
+                <h1 className="shoppingCart-productPrice-flatrate">Flat Rate: 10$</h1>
+                <div className="shoppingCart-productPrice-total">
+                  <h1 className="shoppingCart-productPrice-heading">Total:</h1>
+                  <h1 className="shoppingCart-productPrice-price">${getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 10}</h1>
+                </div>
+              </div>
+              <div className="shoppingCart-productPrice-buttons">
+                <Link>
+                  <button className="btn-5">Proceed to check out</button>
+                </Link>
+                <Link to="/cloths">
+                  <button className="btn-5">Continue Shopping</button>
+                </Link>
+              </div>
             </div>
           </div>
-          <button>PROCEED TO CHECKOUT</button>
-        </div>
-        <div className="cartitems-promocode">
-          <p>If you have a promo code, Enter it Here</p>
-          <div className="cartitems-promobox">
-            <input type="text" placeholder="promo code" />
-            <button>Submit</button>
+          <div className="shoppingCart-discount-container">
+            <h1>DISCOUNT</h1>
+            <p>
+              To apply your discount enter number of your coupon
+              <br />
+              and press "SUBMIT" button
+            </p>
+            <div>
+              <input type="text" />
+              <button type="button">Submit</button>
+            </div>
           </div>
-        </div>
-      </div>
-    </div>
+        </section>
+      </>
+    )
   );
 };
 
